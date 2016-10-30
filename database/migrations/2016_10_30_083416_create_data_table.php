79c34acc82712b22f08b563f1920a09da3c474b9
drkwolf <drkwolf@gmail.com>
2016-10-30 11:18:02 +0100

    migration table for data

---
+++ database/migrations/2016_10_30_083416_create_data_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->json('friends');
+            $table->json('friends_recent');
+            $table->json('messages');
+            $table->json('user_info');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+}
