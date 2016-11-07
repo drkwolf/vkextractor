4d95105d1ef4dea9340a739e143ede284689b967
drkwolf <drkwolf@gmail.com>
2016-11-07 21:59:08 +0100

    Fix updated/created _at null in json respone

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,12 @@ class UserController extends Controller
     public function getData() {
         $user = \Auth::user();
         $data = $user->data()->first();
-        $res = [];
-        foreach($data->toArray() as $item => $value) {
-            $res[$item] = json_decode($value);
-        }
-        $res['user_info'] = $res['user_info'][0]; // TODO hack!
-        return response()->json(['response' => $res ]);
+        $res = $data->toArray();
+//        foreach($res as $item => $value) {
+//            $res[$item] = json_decode($value);
+//        }
+//        $res['user_info'] = $res['user_info'][0]; // TODO hack!
+        return response()->json(['response' => $data->toArray() ]);
     }
 
     public function getUpdate() {
--- app/Models/Data.php
+++ app/Models/Data.php
@@ -9,10 +9,29 @@ use Illuminate\Database\Eloquent\Model;
 class Data extends Model
 {
 
-    protected $fillable = ['friends', 'friends_recent', 'messages', 'user_info'];
+  protected $fillable = ['friends', 'friends_recent', 'messages', 'user_info'];
 
-    public function user()
-    {
-        return $this->belongsTo('App\Models\User');
-    }
+  protected $hidden = ['id', 'user_id'];
+
+  public function user()
+  {
+    return $this->belongsTo('App\Models\User');
+  }
+
+  public function getFriendsAttribute($value) {
+    return json_decode($value);
+  }
+  public function getFriendsRecentAttribute($value) {
+    return json_decode($value);
+  }
+
+
+  public function getMessagesAttribute($value) {
+    return json_decode($value);
+  }
+
+  public function getUserInfoAttribute($value) {
+    $data = json_decode($value);
+    return $data[0] ; // FIXME HACK
+  }
 }
