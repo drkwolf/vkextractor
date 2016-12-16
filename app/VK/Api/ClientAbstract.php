8ff8a4607be074de7920dc8cd879ecba459fd68b
drkwolf <drkwolf@gmail.com>
2016-12-16 14:06:58 +0100

    print progress

--- app/VK/Api/ClientAbstract.php
+++ app/VK/Api/ClientAbstract.php
@@ -209,7 +209,7 @@ abstract class ClientAbstract implements ClientInterface
     } catch(GuzzleException $e) {
       // TODO serialise obeset then resume after connection comes
       $this->failureLog->error($e);
-      $this->failureLog->error('param '.$params);
+      $this->failureLog->error($default);
       throw $e;
     }
 
